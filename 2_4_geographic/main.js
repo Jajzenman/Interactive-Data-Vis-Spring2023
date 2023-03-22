34229ea823b60f2f1ffe97192ab3720707f1c6d9
Jajzenman <jajzenman@gmail.com>
2023-03-22 17:01:55 -0400

    Update main.js
    recopied js from visual code

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,6 +1,7 @@
-/* 2-4 geographic */
+/* 2_4_geographic */
 
 /* CONSTANTS AND GLOBALS */
+
 const width = window.innerWidth * 0.9,
     height = window.innerHeight * 0.7,
     margin = { top: 20, bottom: 50, left: 60, right: 40 };
@@ -29,7 +30,6 @@ Promise.all([
             width - margin.left - margin.right,
             height - margin.top - margin.bottom
         ], geojson);
-
 
     // DEFINE PATH FUNCTION
     const path = d3.geoPath(projection)
@@ -79,5 +79,4 @@ Promise.all([
             return `translate(${x}, ${y})`
         }) //projection converts lat/long from the heatextremes dataset into x/y coordinates
 
-
- 
+});
